ca388fe8f5fe5efee922148945704abb55b81a60
Matthias Ender <matthias.ender@sas.com>
2017-08-30 16:41:34 -0400

    add raiding of ephemeral drives

--- scripts/diskprep.sh
+++ scripts/diskprep.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -e
-set -x
 
 
-METADATA_URL_BASE="http://169.254.169.254/latest"
+yum -y -d0 install mdadm
 
+# pin the metadata version to avoid surprise updates
+METADATA_URL_BASE="http://169.254.169.254/2012-01-12"
+
+
+# determine the device naming scheme (xvd vs sd)
 root_drive=$(df -h | grep -v grep | awk 'NR==2{print $1}')
-
 if [ "${root_drive:0:8}" == "/dev/xvd" ]; then
   echo "Detected 'xvd' drive naming scheme (root: $root_drive)"
   DRIVE_SCHEME='xvd'
@@ -29,9 +32,9 @@ for e in $ebss; do
     dir="/opt/sas"
     mkdir -p $dir
     # format if needed
-    [ "$(blkid $device_path | grep ext4)" = "" ] && mkfs.ext4 $device_path
+    [ "$(blkid $device_path | grep xfs)" = "" ] && mkfs.xfs $device_path
     # add to fstab if needed and mount
-    FSTAB="$device_path      $dir   ext4    defaults,nofail        0       2"
+    FSTAB="$device_path      $dir   xfs    defaults,nofail        0       2"
     ! (grep "$FSTAB" /etc/fstab) &&  echo "$FSTAB" | tee -a /etc/fstab
     umount $dir || true
     mount $dir
@@ -42,36 +45,71 @@ for e in $ebss; do
 done
 
 
-ephemeral_count=0
-ephemerals=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/ | grep ephemeral || true)
-for e in $ephemerals; do
-  ephemeral_count=$((ephemeral_count+1))
-  device_name=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/$e)
-  device_path="/dev/nvme0n$ephemeral_count"
+# format (raid) ephemeral drives if needed
+if  [ "$(blkid /dev/md0 | grep xfs)" = "" ]; then
 
-  # mount nvme0n1 to /sastmp
-  if [[ "$device_name" == "sdb" ]]  && [ -b "$device_path" ] ; then
-    dir="/sastmp"
-    mkdir -p $dir
-    # format if needed
-    [ "$(blkid $device_path | grep ext4)" = "" ] && mkfs.ext4 -E nodiscard $device_path
-    # add to fstab if needed and mount
-    FSTAB="$device_path $dir ext4 defaults,nofail,discard 0 2"
-    ! (grep "$FSTAB" /etc/fstab) &&  echo "$FSTAB" | tee -a /etc/fstab
-    umount $dir || true
-    mount $dir
+  drives=""
+  drive_count=0
+  nvm_drives=$(lsblk  -d -n --output NAME | grep nvm)
 
-    # TODO move this into pre.deployment.yml
-    chmod 777 ${dir}
-    mkdir -p ${dir}/cascache
-    mkdir -p ${dir}/saswork
- #   chown cas ${dir}/cascache
-    chmod 777 ${dir}/cascache
-#    chown sas ${dir}/saswork
-    chmod 777 ${dir}/saswork
+
+  for device_name in $nvm_drives; do
+
+    device_path="/dev/$device_name"
+
+    if [ -b $device_path ]; then
+      echo "Detected ephemeral disk: $device_path"
+      drives="$drives $device_path"
+      drive_count=$((drive_count + 1 ))
+    else
+      echo "Ephemeral disk $device_path is not present. skipping"
+    fi
+
+  done
+
+  if [ "$drive_count" = 0 ]; then
+    echo "No ephemeral disk detected. exiting"
+    exit 0
   fi
 
-  # TODO: additional ephemeral drives for multi-path saswork/cascache
+  # in case it was mounted already...
+  umount /sastmp || true
+  # for some instances, /mnt is the default instance store, already mounted. so we unmount it:
+  umount /mnt || true
 
-done
 
+  # create RAID and filesystem if needed
+
+  if [ "$(blkid /dev/md0 | grep xfs)" = "" ]; then
+
+    # overwrite first few blocks in case there is a filesystem, otherwise mdadm will prompt for input
+    for drive in $drives; do
+      dd if=/dev/zero of=$drive bs=4096 count=1024
+    done
+
+    READAHEAD=16384
+    partprobe
+    mdadm --create --verbose /dev/md0 --level=0 -c256 --force --raid-devices=$drive_count $drives
+    echo DEVICE $drives | tee /etc/mdadm.conf
+    mdadm --detail --scan | tee -a /etc/mdadm.conf
+    blockdev --setra $READAHEAD /dev/md0
+
+    mkfs -t xfs /dev/md0
+  fi
+
+  if [ ! -d /sastmp/ ]; then
+      mkdir /sastmp
+  fi
+
+  mount -t xfs -o noatime /dev/md0 /sastmp
+
+  if [ ! -d /sastmp/saswork/ ]; then
+      mkdir /sastmp/saswork
+  fi
+  chmod 777 /sastmp/saswork
+  if [ ! -d /sastmp/cascache/ ]; then
+      mkdir /sastmp/cascache
+  fi
+  chmod 777 /sastmp/cascache
+
+fi
