ca7fe1711c58cd2762dcf45e7bfdbc57f819e0da
Matthias Ender <matthias.ender@sas.com>
2017-08-30 12:48:09 -0400

    add openldap copied from https://gitlab.sas.com/canepg/OpenLDAP

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,9 +4,24 @@ echo VIYA_SERVICES_NODE_IP=$VIYA_SERVICES_NODE_IP
 echo CAS_CONTROLLER_NODE_IP=$CAS_CONTROLLER_NODE_IP
 
 
-# prepate inventory.ini header
-echo deployTarget ansible_ssh_host=$VIYA_SERVICES_NODE_IP > /tmp/stackinv.ini
-echo controller ansible_ssh_host=$CAS_CONTROLLER_NODE_IP >> /tmp/stackinv.ini
+# prepare inventory.ini header
+echo deployTarget ansible_ssh_host=$VIYA_SERVICES_NODE_IP > /tmp/inventory.head
+echo controller ansible_ssh_host=$CAS_CONTROLLER_NODE_IP >> /tmp/inventory.head
+
+# set up OpenLDAP
+pushd openldap
+
+  # link deployment log file into web server
+  touch openldap-deployment.log
+  sudo ln openldap-deployment.log /var/www/html/status/openldap-deployment.log
+
+  ansible-playbook update.inventory.yml
+
+  ansible-playbook openldapsetup.yml
+
+popd
+
+
 
 # untar playbook
 tar xf /tmp/SAS_Viya_playbook.tgz
@@ -25,6 +40,8 @@ pushd sas_viya_playbook
   ansible-playbook ansible.pre.deployment.yml
 
   ansible-playbook ansible.update.vars.file.yml
+
+  cp ../openldap/sitedefault.yml roles/consul/files/
 
   ansible-playbook site.yml
 
