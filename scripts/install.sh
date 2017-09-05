94a60441ac0fec945ed1e93a7eb90ad32254101d
Matthias Ender <matthias.ender@sas.com>
2017-09-05 15:59:27 -0400

    put deployment logs into CloudWatch -also: use Moustache parameters for install.sh - also: prefix Instance names with stack name

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# this script is expect to be run by a user with sudo privileges (typically ec2-user)
+
 set -o pipefail
 set -o errexit
 set -o nounset
 
-echo VIYA_SERVICES_NODE_IP=$VIYA_SERVICES_NODE_IP
-echo CAS_CONTROLLER_NODE_IP=$CAS_CONTROLLER_NODE_IP
 
-# sometimes there are download failures during the install
+# This is a mustache template.
+# Make sure all the input parms are set
+test -n "{{ViyaServicesNodeIP}}"
+test -n "{{CASControllerNodeIP}}"
+test -n "{{SASViyaAdminPassword}}"
+test -n "{{LogGroup}}"
+
+
+# sometimes there are ssh connection errors (53) during the install
+# this function allows to retry N times
 try () {
   # allow up to N attempts of a command
   # syntax: try N [command]
@@ -21,20 +30,41 @@ try () {
   done
 }
 
+addLogFileToCloudWatch () {
+
+# add file to CloudWatch configuration and restart CloudWatch agent
+
+LOGFILE=$1
+cat <<-EOF | sudo tee -a /etc/awslogs/awslogs.conf
+
+[$LOGFILE]
+log_stream_name = $LOGFILE
+initial_position = start_of_file
+file = $(pwd)/$LOGFILE
+log_group_name = {{LogGroup}}
+EOF
+
+sudo service awslogs restart
+
+}
+
 
 # prepare inventory.ini header
-echo deployTarget ansible_ssh_host=$VIYA_SERVICES_NODE_IP > /tmp/inventory.head
-echo controller ansible_ssh_host=$CAS_CONTROLLER_NODE_IP >> /tmp/inventory.head
+echo deployTarget ansible_ssh_host={{ViyaServicesNodeIP}} > /tmp/inventory.head
+echo controller ansible_ssh_host={{CASControllerNodeIP}} >> /tmp/inventory.head
 
 # set up OpenLDAP
 pushd openldap
 
-  # link deployment log file into web server
-  touch openldap-deployment.log
-  sudo ln openldap-deployment.log /var/www/html/status/openldap-deployment.log
+  # set log file
+  export ANSIBLE_LOG_PATH=openldap-deployment.log
+  touch $ANSIBLE_LOG_PATH
+  addLogFileToCloudWatch $ANSIBLE_LOG_PATH
 
+  # add hosts
   ansible-playbook update.inventory.yml
 
+  # openldap and sssd setup
   ansible-playbook openldapsetup.yml
 
 popd
@@ -46,24 +76,32 @@ tar xf /tmp/SAS_Viya_playbook.tgz
 
 pushd sas_viya_playbook
 
-  # link deployment log file into web server
-  touch deployment.log
-  sudo ln deployment.log /var/www/html/status/deployment.log
-
   # copy additional playbooks
   mv /tmp/ansible.* .
 
-  ansible-playbook ansible.update.inventory.yml
-
-  try 3 ansible-playbook ansible.pre.deployment.yml
-
-  ansible-playbook ansible.update.vars.file.yml
-
+  # get identities configuration from openldap setup
   cp ../openldap/sitedefault.yml roles/consul/files/
 
-  try 3 ansible-playbook site.yml
+  # set log file for pre deployment steps
+  export ANSIBLE_LOG_PATH=viya-pre-deployment.log
+  touch $ANSIBLE_LOG_PATH
+  addLogFileToCloudWatch $ANSIBLE_LOG_PATH
 
-  ansible-playbook ansible.post.deployment.yml -e "sasboot_pw=$SASBOOT_PW"
+  # add hosts to inventory
+  ansible-playbook ansible.update.inventory.yml
+
+  # set prereqs on hosts
+  try 3 ansible-playbook ansible.pre.deployment.yml
+
+  # set log file for main deployment
+  export ANSIBLE_LOG_PATH=viya-deployment.log
+  touch $ANSIBLE_LOG_PATH
+  addLogFileToCloudWatch $ANSIBLE_LOG_PATH
+
+  # main deployment
+  ansible-playbook ansible.update.vars.file.yml
+  try 3 ansible-playbook site.yml
+  ansible-playbook ansible.post.deployment.yml -e "sasboot_pw={{SASViyaAdminPassword}}"
 
 popd
 
