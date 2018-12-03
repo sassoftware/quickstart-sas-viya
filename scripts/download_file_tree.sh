7f7ceb81af74821a4dee3696fed6f329af163125
enderm <matthias.ender@sas.com>
2018-12-03 13:33:14 -0500

    (AWSVIYA-103) add sns messages and parm validation - replace common/download/download_file_tree.sh with faster code - add pystache file postprocessing - add send_sns_message.sh script - remove ssl cert and dns verification (better and faster by CFN) - add Mirror validation in validate_parameters.sh - make some ansible controller iam policies conditional - clean up ansible controller policies syntax
    Change-Id: I62fe1516c735b17facadf477039a902c788532f8

---
+++ scripts/download_file_tree.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This script is part of the ansible controller preparation.
+#
+# It downloads script files and ansible playbook from the project store
+#
+# The script expects the following environment variables to be set:
+#
+# FILE_ROOT - the IAAS location of  the  project files (AWS default aws-quickstart/quickstart-sas-viya)
+#
+# This script overrrides the implementation of common/scripts/download_file_tree.sh
+# That script download each file individually. That takes up to 1:30 minutes, vs. just a second when
+# using "cp --recursive"
+
+
+set -e
+
+test -n $FILE_ROOT
+DOWNLOAD_DIR=/sas/install
+INSTALL_USER=$(whoami)
+
+echo Downloading from ${FILE_ROOT} as ${INSTALL_USER}
+
+pushd $DOWNLOAD_DIR
+
+   aws s3 cp --recursive s3://${FILE_ROOT} . \
+      --exclude 'templates/*' \
+      --exclude 'doc/*'  \
+      --exclude 'images/*' \
+      --exclude '*file_tree*' \
+      --exclude '*/README.md' \
+      --exclude '*/.*' \
+      --exclude 'ci/*'
+
+   chown -R ${INSTALL_USER}:${INSTALL_USER} .
+   chmod -R 755 ansible bin common scripts
+popd
+
+
+
+
--- scripts/send_sns_message.sh
+++ scripts/send_sns_message.sh
@@ -22,8 +22,9 @@ FRC=${RC}
 #
 # "Starting SAS Viya Deployment" message
 #
-START_SUBJECT="Starting SAS Viya Deployment {{StackName}}"
-START_MESSAGE='
+if [ $TYPE = starting ]; then
+SUBJECT="Starting SAS Viya Deployment {{StackName}}"
+MESSAGE='
 
   Starting SAS Viya Deployment for Stack "{{StackName}}".
 
@@ -44,36 +45,58 @@ START_MESSAGE='
 #
 #  "completed successfully" message
 #
-SUCCESS_SUBJECT="SAS Viya Deployment completed for Stack {{StackName}}"
-SUCCESS_MESSAGE='
+elif [ $TYPE = success ]; then
+    if [ -z "{{DomainName}}" ]; then
+      EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+      EC2_REGION=$(echo ${EC2_AVAIL_ZONE}  | sed "s/[a-z]$//")
+      ID=$(aws --no-paginate --region "$EC2_REGION" cloudformation describe-stack-resources --stack-name "{{StackName}}" --logical-resource-id ElasticLoadBalancer --query StackResources[*].PhysicalResourceId --output text)
+      DomainName=$(aws --no-paginate --region "$EC2_REGION" elb describe-load-balancers --load-balancer-name "$ID" --query LoadBalancerDescriptions[*].DNSName --output text)
+    else
+      DomainName="{{DomainName}}"
+    fi
 
-   SAS Viya Deployment for Stack "{{StackName}}" completed successfully.
+    #
+    # set the SASDrive and SASStudio urls
+    #
+    SASDrive="https://${DomainName}/SASDrive"
+    SASStudio="https://${DomainName}/SASStudioV"
 
-   Log into SAS Viya at {{SASDrive}}
 
-   Log into SAS Studio at {{SASStudio}}
+    SUBJECT="SAS Viya Deployment completed for Stack {{StackName}}"
+    MESSAGE="
 
-   For administrative tasks:
+       SAS Viya Deployment for Stack \"{{StackName}}\" completed successfully.
 
-     See the deployment and application logs at {{CloudWatchLogs}}
+       Log into SAS Viya at:
+         $SASDrive
 
-     Log into the Ansible Controller VM with the private key for KeyPair "{{KeyPairName}}":
+       Log into SAS Studio at:
+         $SASStudio
 
-       ssh -i /path/to/private/key.pem ec2-user@{{AnsibleControllerIP}}
+       For administrative tasks:
 
-     From the ansible controller, you can ssh into these VMs:
+         See the deployment and application logs at {{CloudWatchLogs}}
 
-       Viya Services:
-         services.viya.sas (services)
-       CAS Controller:
-         controller.viya.sas (controller)
-'
+         Log into the Ansible Controller VM with the private key for KeyPair \"{{KeyPairName}}\":
+
+           ssh -i /path/to/private/key.pem ec2-user@{{AnsibleControllerIP}}
+
+         From the ansible controller, you can ssh into these VMs:
+
+           Viya Services:
+             services.viya.sas (services)
+           CAS Controller:
+             controller.viya.sas (controller)
+    "
+
 
 #
 # "failed" message
 #
-FAILURE_SUBJECT="SAS Viya Deployment failed for Stack {{StackName}}"
-FAILURE_MESSAGE="
+elif [ $TYPE = failure ]; then
+
+SUBJECT="SAS Viya Deployment failed for Stack {{StackName}}"
+MESSAGE="
 
    SAS Viya Deployment for Stack "{{StackName}}" failed with RC=$FRC.
 
@@ -87,15 +110,6 @@ FAILURE_MESSAGE="
 "
 
 
-if [[ $TYPE == starting ]]; then
-  SUBJECT=$START_SUBJECT
-  MESSAGE=$START_MESSAGE
-elif [[ $TYPE == success ]]; then
-  SUBJECT=$SUCCESS_SUBJECT
-  MESSAGE=$SUCCESS_MESSAGE
-elif [[ $TYPE == failure ]]; then
-  SUBJECT=$FAILURE_SUBJECT
-  MESSAGE=$FAILURE_MESSAGE
 else
   exit 1
 fi
