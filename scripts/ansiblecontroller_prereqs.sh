1ea5d9cd65d254f94627bd363ef3206f910843e9
Matthias Ender <matthias.ender@sas.com>
2019-03-05 13:10:23 -0500

    (AWSVIYA-103) include common code from AWSVIYA-1.4 tag

---
+++ scripts/ansiblecontroller_prereqs.sh
@@ -0,0 +1,118 @@
+#!/bin/bash -e
+
+set -x
+
+# Run this script as install user (e.g. ec2-user on aws)
+
+INSTALL_DIR=/sas/install
+NFS_SHARE_DIR=${INSTALL_DIR}/nfs
+ANSIBLE_KEY_DIR=${NFS_SHARE_DIR}/ansible_key
+READINESS_FLAGS_DIR=${NFS_SHARE_DIR}/readiness_flags
+LOGS_DIR=/var/log/sas/install
+INSTALL_USER=$(whoami)
+UTILITIES_DIR="${INSTALL_DIR}/bin"
+#
+# create directories
+#
+sudo mkdir -p ${INSTALL_DIR}
+sudo chmod 755 ${INSTALL_DIR}
+sudo chown ${INSTALL_USER}:${INSTALL_USER} ${INSTALL_DIR}
+
+sudo mkdir -p ${NFS_SHARE_DIR}
+sudo chmod 777 ${NFS_SHARE_DIR}  # may not need to be 777 since it should be the same user everywhere. The user may have a different UID/GUI though.
+sudo chown ${INSTALL_USER}:${INSTALL_USER} ${NFS_SHARE_DIR}
+
+sudo mkdir -p ${ANSIBLE_KEY_DIR}
+sudo chmod 777 ${ANSIBLE_KEY_DIR}  # may not need to be 777 since it should be the same user everywhere. The user may have a different UID/GUI though.
+sudo chown ${INSTALL_USER}:${INSTALL_USER} ${ANSIBLE_KEY_DIR}
+
+sudo mkdir -p ${READINESS_FLAGS_DIR}
+sudo chmod 777 ${READINESS_FLAGS_DIR}   # may not need to be 777 since it should be the same user everywhere. The user may have a different UID/GUI though.
+sudo chown ${INSTALL_USER}:${INSTALL_USER} ${READINESS_FLAGS_DIR}
+
+sudo mkdir -p ${LOGS_DIR}
+sudo chmod 755 ${LOGS_DIR}
+sudo chown ${INSTALL_USER}:${INSTALL_USER} ${LOGS_DIR}
+
+sudo mkdir -p ${UTILITIES_DIR}
+sudo chmod 755 ${UTILITIES_DIR}
+sudo chown ${INSTALL_USER}:${INSTALL_USER} ${UTILITIES_DIR}
+
+#
+# Install or upgrade java to 1.8
+#
+install_java () {
+   echo Install java 1.8
+   sudo yum -y install java-1.8.0
+}
+
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME
+    _java="$JAVA_HOME/bin/java"
+else
+    install_java
+fi
+
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    if [[ "$version" < "1.8" ]]; then
+        sudo yum -y remove java
+        install_java
+    else
+        echo version 1.8 or greater
+    fi
+fi
+
+#
+# install ansible
+#
+if ! type -p ansible;  then
+   # install Ansible
+   sudo pip install 'ansible==2.4.3'
+fi
+
+#
+# install git
+#
+if ! type -p git; then
+   # install git
+   sudo yum install -y git
+fi
+
+#
+# NFS setup
+#
+echo -n "${NFS_SHARE_DIR} *(rw,sync)" | sudo tee /etc/exports
+sudo yum install -y nfs-utils nfs-utils-lib
+
+if ! type -p systemctl; then
+    sudo service rpcbind start
+    sudo /sbin/chkconfig --add rpcbind
+    sudo service nfs start
+    sudo /sbin/chkconfig --add nfs
+else
+    sudo systemctl start rpcbind
+    sudo systemctl enable rpcbind
+    sudo systemctl start nfs
+    sudo systemctl enable nfs
+fi
+
+
+#
+#  Ansible Key
+#
+ssh-keygen -t rsa -f ~/.ssh/id_rsa -q -N ''
+cp ~/.ssh/id_rsa.pub ${ANSIBLE_KEY_DIR}/id_rsa.pub
+
+
+
+
+
+
+
+
---
+++ scripts/sasnodes_prereqs.sh
@@ -0,0 +1,76 @@
+#!/bin/bash -e
+
+# Run this script as root or sudo
+# Set the following environment variables:
+#   NFS_SERVER (the ip or dns of the bastion host )
+#   HOST (the label/alias for the machine - services|controller)
+
+BASTION_NFS_SHARE="/sas/install/nfs"
+NFS_MOUNT_POINT="/mnt/ansiblecontroller"
+ANSIBLE_KEY="${NFS_MOUNT_POINT}/ansible_key/id_rsa.pub"
+NFS_SERVER=${NFS_SERVER:-ansible}
+READINESS_FLAG_FILE="${NFS_MOUNT_POINT}/readiness_flags/${HOST}"
+
+#
+# create mount dir
+#
+sudo mkdir -p ${NFS_MOUNT_POINT}
+
+#
+# install and start nfs
+#
+sudo yum install -y nfs-utils
+sudo systemctl start nfs
+sudo systemctl enable nfs
+
+#
+# mount nfs share
+#
+MOUNT="${NFS_SERVER}:${BASTION_NFS_SHARE}  ${NFS_MOUNT_POINT} nfs rw,hard,intr,bg 0 0"
+grep -q "$MOUNT" /etc/fstab || echo "$MOUNT" | sudo tee -a /etc/fstab
+
+
+sudo mount "${NFS_MOUNT_POINT}"
+RET=$?
+
+#
+# the ansible controller should already be available and fully initialized at this point
+# since we need its ip address or fqdn, but the following should address
+# any asynchronous scenarios
+#
+while [ "$RET" -gt "0" ]; do
+	echo "Waiting 5 seconds for mount to be possible"
+	sleep 5
+	sudo mount "${NFS_MOUNT_POINT}"
+	RET=$?
+done
+echo "Mounting Successful"
+
+#
+# again, this may be needed if the ansible controller and the viya vms initialize in parallel
+#
+wait_count=0
+stop_waiting_count=600
+while [ ! -e "$ANSIBLE_KEY" ]; do
+	echo "waiting 5 seconds for key to come around"
+	sleep 1
+	if [ "$((wait_count++))" -gt "$stop_waiting_count" ]; then
+		exit 1
+	fi
+done
+
+
+#
+# copy public ansible key from NFS share
+#
+mkdir -p ~/.ssh
+cat ${ANSIBLE_KEY} >> ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+#
+# post readiness flag
+#
+LOCALIP=$(ip -o -f inet addr | grep eth0 | sed -r 's/.*\b(([0-9]{1,3}\.){3}[0-9]{1,3})\/.*/\1/g')
+echo $LOCALIP > $READINESS_FLAG_FILE
+
+
