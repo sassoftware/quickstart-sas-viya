80c9d966505e390aba4e47a7489f6cb66ec41610
Gary W. Black <gary.black@sas.com>
2019-07-02 12:57:17 -0400

    (AWSVIYA-194) Add ability to override viya-ark location
    This commit updates process_overrides.sh to allow users to override the
    location from which viya-ark is obtained. See process_overrides.sh for details.

--- scripts/process_overrides.sh
+++ scripts/process_overrides.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+EC2_AVAIL_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+EC2_REGION=$(echo ${EC2_AVAIL_ZONE}  | sed "s/[a-z]$//")
+
 #
 # process overrides
 #
@@ -7,13 +10,17 @@
 # common code and orchestration changes that are not yet in production.
 #
 # To override the common code URL:
-#    Add a file named "common_code.sh" to the root S3 folder containing the quickstart.
-#    This file should have a single line of the form:
+#    Add a file named "git_overrides.sh" to the root S3 folder containing the quickstart.
+#    This file can contain the following variables have a single line of the form:
 #
 #       COMMON_CODE_BRANCH=<git branch name>
+#       VIYA_ARK_URL=<s3 location for Viya Ark>
 #
-#    Where <git branch name> is the name of the branch in the GitHub common code repository to be used
-#    by the deployment.
+#    Where :
+#       <git branch name> is the name of the branch in the GitHub common code repository to be used
+#          by the deployment.
+#       <s3 location for Viya Ark> is the s3 URL pointing to a Viya Ark folder in S3 (to be used when testing
+#          a Viya Ark change that is not yet publicly available from GitHub)
 #
 # To override the SAS Orchestration CLI URL:
 #    Add a file named "group_vars.yaml" to the root S3 folder containing the quickstart.
@@ -27,10 +34,12 @@
 
 # Check for common code url override file - if this file exists, open it to get the correct
 # common code tag
+INSTALL_USER=$(whoami)
+
 pushd /sas/install
-COMMON_CODE_OVERRIDE="common_code.sh"
-if [ -f "$COMMON_CODE_OVERRIDE" ]; then
-    source "$COMMON_CODE_OVERRIDE"
+GIT_OVERRIDE="git_overrides.sh"
+if [ -f "$GIT_OVERRIDE" ]; then
+    source "$GIT_OVERRIDE"
     if [[ -n $COMMON_CODE_BRANCH ]]; then
 	rm -Rf common
 	git clone https://github.com/sassoftware/quickstart-sas-viya-common.git common
@@ -41,6 +50,21 @@ if [ -f "$COMMON_CODE_OVERRIDE" ]; then
 	    echo "ERROR: Common code branch $COMMON_CODE_BRANCH cannot be checked out"
 	    exit $RC
 	fi
+	echo "Overriding common code branch with branch: $COMMON_CODE_BRANCH"
+    fi
+    if [[ -n $VIYA_ARK_URL ]]; then
+	VIRK_DIR="/sas/install/ansible/sas_viya_playbook/viya-ark"
+	rm -Rf $VIRK_DIR
+	aws --region ${EC2_REGION} s3 sync $VIYA_ARK_URL $VIRK_DIR
+	chown -R ${INSTALL_USER}:${INSTALL_USER} ansible
+	
+	RC=$?
+	
+	if [ ! $RC = 0 ]; then
+	    echo "ERROR: Could not override Viya Ark with $VIYA_ARK_URL"
+	    exit $RC
+	fi
+	echo "Overriding Viya Ark location with $VIYA_ARK_URL"
     fi
 fi
    
@@ -56,5 +80,6 @@ if [[ -f "$GROUP_VAR_OVERRIDE" ]]; then
 	echo "ERROR: group_vars.yaml override file cannot be processed."
 	exit $RC
     fi
+    echo "Overriding group vars"
 fi
 popd
